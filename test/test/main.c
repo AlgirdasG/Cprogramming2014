7b4a95b74e90219a32a770d692e395218b07bf86
Algirdas G <algirdas.galaunia@gmail.com>
2014-02-24 21:45:45 +0200

    TESTAS

---
+++ test/test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Algirdas on 24/02/14.
+//  Copyright (c) 2014 Algirdas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
